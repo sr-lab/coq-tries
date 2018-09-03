40c865c7c50661646ffdaa3c5086a79c3d7313dc
Saul Johnson <saul.a.johnson@gmail.com>
2018-09-03 20:15:53 +0100

    Document example

--- Example.v
+++ Example.v
@@ -4,6 +4,8 @@ Load Trie.
 
 Open Scope char_scope.
 
+(* Let's add a few words to a trie mapping lists of ASCII characters to natural numbers. *)
+
 Definition empty := empty_trie ascii nat.
 
 Definition hello := trie_insert ascii_dec empty ["h"; "e"; "l"; "l"; "o"] 1.
@@ -12,6 +14,10 @@ Definition happy := trie_insert ascii_dec hello ["h"; "a"; "p"; "p"; "y"] 2.
 
 Definition world := trie_insert ascii_dec happy ["w"; "o"; "r"; "l"; "d"] 3.
 
+(* Compute the whole trie. *)
+
 Compute world.
+
+(* Grab a value from the tree. *)
 
 Compute trie_get ascii_dec world ["h"; "a"; "p"; "p"; "y"].
